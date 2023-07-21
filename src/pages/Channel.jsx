15550aff1f3c60e87ba36e4bc7ddf68772243082
AlexMoreni <alex.moreni47@gmail.com>
2023-07-21 15:04:53 -0300

    Inserting Links in the channel card

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -18,20 +18,40 @@ const Channel = () => {
   return (
     <section className={styles.container_channels}>
       <div className={styles.container_list_channels}>
-        <Card img={Globo} alt="Globo" name="Globo" />
-        <Card img={SBT} alt="SBT" name="SBT" />
-        <Card img={Viva} alt="Viva" name="Viva" />
-        <Card
-          img={Discovery}
-          alt="Discovery channel"
-          name="Discovery channel"
-        />
-        <Card img={Animal} alt="Animal Planet" name="Animal Planet" />
-        <Card img={Telecine} alt="Telecine Premium" name="Telecine Premium" />
-        <Card img={ESPN} alt="ESPN" name="ESPN" />
-        <Card img={Combate} alt="Combate" name="Combate" />
-        <Card img={Cartoon} alt="Cartoon" name="Cartoon" />
-        <Card img={Disney} alt="Disney" name="Disney" />
+        <a href="https://cinexus-bobosp.netlify.app">
+          <Card img={Globo} alt="Globo" name="Globo" />
+        </a>
+        <a href="https://cinexus-sbt.netlify.app">
+          <Card img={SBT} alt="SBT" name="SBT" />
+        </a>
+        <a href="https://cinexus-viva.netlify.app">
+          <Card img={Viva} alt="Viva" name="Viva" />
+        </a>
+        <a href="https://cinexus-discovery.netlify.app">
+          <Card
+            img={Discovery}
+            alt="Discovery channel"
+            name="Discovery channel"
+          />
+        </a>
+        <a href="https://cinexus-animal-planet.netlify.app">
+          <Card img={Animal} alt="Animal Planet" name="Animal Planet" />
+        </a>
+        <a href="https://cinexus-telecinepr.netlify.app">
+          <Card img={Telecine} alt="Telecine Premium" name="Telecine Premium" />
+        </a>
+        <a href="https://cinexus-espn.netlify.app">
+          <Card img={ESPN} alt="ESPN" name="ESPN" />
+        </a>
+        <a href="https://cinexus-combate.netlify.app">
+          <Card img={Combate} alt="Combate" name="Combate" />
+        </a>
+        <a href="https://cinexus-cartoon.netlify.app">
+          <Card img={Cartoon} alt="Cartoon" name="Cartoon" />
+        </a>
+        <a href="https://cinexus-disney.netlify.app">
+          <Card img={Disney} alt="Disney" name="Disney" />
+        </a>
       </div>
     </section>
   );
