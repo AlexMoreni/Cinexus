5ce44b8c1b3c33e6db7be0282e6e013656c1e343
AlexMoreni <alex.moreni47@gmail.com>
2023-07-21 15:40:25 -0300

    Creating the structure and styling of the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import styles from "./Home.module.css";
-
-import Elementos from "../../public/video/elementos.mp4";
-
 import { useEffect, useState } from "react";
+
+import Card from "../components/layout/Card";
+
+//Canais de TV
+import Globo from "../../public/video player/Channels/img/Globo.jpeg";
+import Animal from "../../public/video player/Channels/img/Animal-Planet.png";
+import Combate from "../../public/video player/Channels/img/Combate.png";
+import Disney from "../../public/video player/Channels/img/Disney.jfif";
+
+//Filmes
 
 const url = "http://localhost:5000/api/canais";
 
@@ -29,23 +36,44 @@ const Home = () => {
   return (
     <section>
       <main className={styles.container_home}>
-        <video src={Elementos} width="60%" controls autoPlay muted loop></video>
-        <ul>
-          {canais.map((canal) => (
-            <li key={canal.id}>
-              <iframe
-                name="Player"
-                src="https://sinalpublico.com/player3/ch.php?canal=agrandeconquista2"
-                frameBorder="0"
-                height="400"
-                scrolling="no"
-                width="640"
-                allowfullscreen
-              ></iframe>
-              <h1>{canal.name}</h1>
-            </li>
-          ))}
-        </ul>
+        <div className={styles.container_textHome}>
+          <h1>Cinexus</h1>
+          <p>Conectando você ao universo cinematográfico</p>
+        </div>
+        <div className={styles.box_content}>
+          <h2>Canais TV</h2>
+          <div className={styles.container_listHome}>
+            <a href="https://cinexus-bobosp.netlify.app">
+              <Card img={Globo} alt="Globo" name="Globo" />
+            </a>
+            <a href="https://cinexus-combate.netlify.app">
+              <Card img={Combate} alt="Combate" name="Combate" />
+            </a>
+            <a href="https://cinexus-disney.netlify.app">
+              <Card img={Disney} alt="Disney" name="Disney" />
+            </a>
+            <a href="https://cinexus-animal-planet.netlify.app">
+              <Card img={Animal} alt="Animal Planet" name="Animal Planet" />
+            </a>
+          </div>
+        </div>
+        <div className={styles.box_content}>
+          <h2>Filmes</h2>
+          <div className={styles.container_listHome}>
+            <a href="https://cinexus-bobosp.netlify.app">
+              <Card img={Globo} alt="Globo" name="Globo" />
+            </a>
+            <a href="https://cinexus-combate.netlify.app">
+              <Card img={Combate} alt="Combate" name="Combate" />
+            </a>
+            <a href="https://cinexus-disney.netlify.app">
+              <Card img={Disney} alt="Disney" name="Disney" />
+            </a>
+            <a href="https://cinexus-animal-planet.netlify.app">
+              <Card img={Animal} alt="Animal Planet" name="Animal Planet" />
+            </a>
+          </div>
+        </div>
       </main>
     </section>
   );
